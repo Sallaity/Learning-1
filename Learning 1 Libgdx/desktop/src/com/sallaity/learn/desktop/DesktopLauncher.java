b32af5ea518f296fd0acd685c72eaace107a3716
Sallaity <64588204+Sallaity@users.noreply.github.com>
2020-05-10 16:30:13 +0100

    Add files via upload
    Just pushing code to coding teacher

---
+++ Learning 1 Libgdx/desktop/src/com/sallaity/learn/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sallaity.learn.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sallaity.learn.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
